3972a7c9aa2ca16bee7e90a53ef7ba4ab60680de
Adineradofn <114971107+Adineradofn@users.noreply.github.com>
2023-09-18 20:49:50 +0800

    Delete PRAKTIKUM1 directory

--- PRAKTIKUM1/src/PRAK104_2210817310003_AdineradoFalahNugraha.java
+++
@@ -1,9 +0,0 @@
-public class PRAK104_2210817310003_AdineradoFalahNugraha {
-    public static void main(String[] args) {
-        String nama = "Adinerado Falah Nugraha";
-        int usia = 19;
-        System.out.print("Perkenalkan Nama Saya "+nama);
-        System.out.print(", Umur Saya "+usia);
-
-    }
-}
