0aee0e3467f04b8af5e3a4b60167f3dcacc45101
Adineradofn <114971107+Adineradofn@users.noreply.github.com>
2023-11-15 14:01:07 +0800

    Add files via upload

---
+++ PRAKTIKUM1/src/PRAK101_2210817310003_AdineradoFalahNugraha.java
@@ -0,0 +1,7 @@
+public class PRAK101_2210817310003_AdineradoFalahNugraha {
+    public static void main(String[] args) {
+        System.out.println("Selamat pagi, Adinerado Falah Nugraha");
+        System.out.println("Selamat Siang, Adinerado Falah Nugraha");
+        System.out.println("Selamat Malam, Adinerado Falah Nugraha");
+    }
+}
